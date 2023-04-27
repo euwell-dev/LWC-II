13ba4ed6771e31ca57fe398b2e5a7ef82c6c8859
Wellington Amorim <130098196+euwell-dev@users.noreply.github.com>
2023-04-27 14:45:26 -0300

    Create Ex02.js

---
+++ Atv02/Ex02.js
@@ -0,0 +1,12 @@
+import { LightningElement } from 'lwc';
+
+export default class ex02 extends LightningElement {
+
+    Opp=[
+        {ide:1, name:'Opp1',amount:2000,stagename:'Prospect'},
+        {ide:2, name:'Opp2',amount:4000,stagename:'Negotiation'},
+        {ide:3, name:'Opp3',amount:6000,stagename:'Closed Won'},
+        {ide:4, name:'Opp4',amount:8000,stagename:'Closed Lost'},
+        {ide:5, name:'Opp5',amount:10000,stagename:'Prospect'},
+    ];
+}
