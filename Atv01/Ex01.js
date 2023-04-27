46bc121269de924b2bde8a41cca1fa79b09239ec
Wellington Amorim <130098196+euwell-dev@users.noreply.github.com>
2023-04-27 14:42:05 -0300

    Create Ex01.js

---
+++ Atv01/Ex01.js
@@ -0,0 +1,18 @@
+import { LightningElement } from 'lwc';
+
+export default class ex01 extends LightningElement {
+
+
+    pessoa = [
+        {
+        id: 345345,
+        primeironome:       "John" , 
+        sobrenome:          "Meyer", 
+        idade:              "35", 
+        email:              "john@meyer.com", 
+        classificacao:      "hot" , 
+        empresa:            "Warner" , 
+        receitaanual:       3000000 
+        }
+        ];
+}
