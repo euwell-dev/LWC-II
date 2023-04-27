e1a99e7d89b5389b73aa096a5eebd92c3c408fac
Wellington Amorim <130098196+euwell-dev@users.noreply.github.com>
2023-04-27 14:20:28 -0300

    Create Ex01.js

---
+++ Ex01.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+
+export default class ex03 extends LightningElement {
+    leaddata = [
+        {ide: 1, firstname: 'Eu sou ', lastname: 'Maria' , status: 'Open - Not Contacted' , phone: 2544945656, email:'maria@comcertidadenascimentosoucidadao.com' , company: 'Eu tenho nome', annualrevenue: 1000000},
+        {ide: 2, firstname: 'Eu sou ', lastname: 'João' , status: 'Working - Contacted' , phone: 5416544264, email:'joao@comcertidadenascimentosoucidadao.com' , company: 'Eu tenho nome', annualrevenue: 200000},       
+       
+    ];
+}
